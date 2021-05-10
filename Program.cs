f04a4d9f6e3a2429fa1c472d2be57d0ad5ccc7e0
Kseniia-ux <79336122+Kseniia-ux@users.noreply.github.com>
2021-05-10 13:58:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4_лаб
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
